d99033ac1c005dd4cc9b70e6d05cb95e2b7d36d3
pkasprzak9 <pawkasprzak9@gmail.com>
2024-07-26 20:19:22 +0200

    ``` Add Main component structure to App
    Integrated Main component into App, including sections for future content expansion. Created SectionCeremony placeholder component and stylesheet for future implementation. These changes set the stage for upcoming features and content additions.
    ```

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SectionCeremony from "./SectionCeremony/SectionCeremony";
+
+export default function Main(){
+    return (
+        <main>
+            <SectionCeremony />
+        </main>
+    );
+}
