e09b04a64e13dc88cfa8fcca5954aebbdfde171d
pkasprzak9 <pawkasprzak9@gmail.com>
2024-07-26 11:19:19 +0200

    ### Commit Message
    ```
    Add ContainerHeader to App component
    
    Integrated ContainerHeader into the App component to enhance UI structure. This adds a new header section, improving navigation and overall layout consistency.
    ```

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ContainerNavBar from './containers/NavBar/ContainerNavBar';
+import ContainerHeader from './containers/Header/ContainerHeader';
 
 function App() {
   return (
     <>
       <ContainerNavBar />
+      <ContainerHeader />
     </>
   );
 }
