5313d091f274bae4fe66264c970c09b43c8857b2
pkasprzak9 <pawkasprzak9@gmail.com>
2024-07-26 10:26:58 +0200

    ``` Improve NavBar responsiveness and layout structure
    Refactored NavBar layout to include a container div for better alignment and spacing of menu items. Adjusted media query breakpoints from 768px to 800px, and added z-index to ensure proper stacking context. Enhanced menu link alignment and spacing for improved responsiveness and user experience. Addressed layout issues with max-width and padding modifications.
    ```

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,29 +6,31 @@ import styles from "./NavBar.module.scss";
 export default function NavBar({ isOpen, setIsOpen }) {
   return (
     <nav className={styles.navbar}>
-      <ul className={`${styles.links} ${isOpen && styles.showMenu}`}>
-        <li>
-          <a href="#later">Ceremonia Ślubu</a>
-        </li>
-        <li>
-          <a href="#later">Przyjęcie</a>
-        </li>
-        <li>
-          <a href="#later">Plan Uroczystości</a>
-        </li>
-        <li>
-          <a href="#later">Potwierdzenie Obecności</a>
-        </li>
-        <li>
-          <a href="#later">FAQ</a>
-        </li>
-        <li>
-          <a href="#later">Kontakt</a>
-        </li>
-      </ul>
       <div className={styles.menuToggle}>
         <FontAwesomeIcon  onClick={setIsOpen} icon={isOpen? faTimes : faBars} className={styles.icon}/>
       </div>
+      <ul className={`${styles.links} ${isOpen && styles.showMenu}`}>
+        <div className={styles.container}>
+          <li>
+            <a href="#later">Ceremonia Ślubu</a>
+          </li>
+          <li>
+            <a href="#later">Przyjęcie</a>
+          </li>
+          <li>
+            <a href="#later">Plan Uroczystości</a>
+          </li>
+          <li>
+            <a href="#later">Potwierdzenie Obecności</a>
+          </li>
+          <li>
+            <a href="#later">FAQ</a>
+          </li>
+          <li>
+            <a href="#later">Kontakt</a>
+          </li>
+        </div>
+      </ul>
     </nav>
   );
 }
