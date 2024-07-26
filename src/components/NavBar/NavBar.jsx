b439be26f9cfa71f3f35f1330e9f304cd3843d28
pkasprzak9 <pawkasprzak9@gmail.com>
2024-07-26 16:32:17 +0200

    ### Subject: Enable closing NavBar on outside click
    ### Body:
    Added logic to handle clicks outside the NavBar component to close the sidebar if it's open. Utilized `useRef` to track the sidebar element and `useEffect` to manage event listener lifecycle, ensuring clean-up to prevent memory leaks. Enhances user experience by allowing intuitive dismissal of the sidebar.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useRef, useEffect} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import styles from "./NavBar.module.scss";
 
 export default function NavBar({ isOpen, setIsOpen }) {
+  const sidebarRef = useRef(null);
+
+  const handleClickOutside = (e) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
+      setIsOpen(false);
+    }
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  }, [isOpen]);
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={styles.navbar} ref={sidebarRef}>
       <div className={styles.menuToggle}>
         <FontAwesomeIcon  onClick={setIsOpen} icon={isOpen? faTimes : faBars} className={styles.icon}/>
       </div>
