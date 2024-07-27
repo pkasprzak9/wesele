40e19ae9d736e05178b9addc85fbb12bfabdaded
pkasprzak9 <pawkasprzak9@gmail.com>
2024-07-27 13:59:42 +0200

    style: Refactor SectionCeremony component for improved layout and styling
    Refactor the SectionCeremony component to improve the layout and styling. This includes removing the image wrapper and image element, and replacing them with a card-like container for the ceremony details. Additionally, update the title font size and style for better visual hierarchy. These changes enhance the overall presentation of the ceremony section and improve the user experience.

--- src/components/Main/SectionCeremony/SectionCeremony.jsx
+++ src/components/Main/SectionCeremony/SectionCeremony.jsx
@@ -20,28 +20,21 @@ export default function SectionCeremony() {
       <div className="container">
         <p className={style.title}>Ślub</p>
         <div className="row">
-          <div className={`column ${style.imageWrapper}`}>
-            <img
-              src={images.church.src}
-              alt={images.church.alt}
-              className={style.image}
-            />
+          <div className={`column ${style.details} ${style.card}`}>
+            <p className={style.dateTime}>
+              <strong>Data<br/></strong> 20 sierpnia 2024<br /><br />
+              <strong>Godzina<br/></strong> 15:00
+            </p>
+            <p className={style.location}>
+              <strong>Miejsce</strong><br />
+              Katedra w Łowiczu<br />
+              ul. Katedralna 1, 99-400 Łowicz
+            </p>
+            <p className={style.description}>
+              Ceremonia odbędzie się w pięknej Katedrze w Łowiczu, znanej ze swojej wyjątkowej architektury i niepowtarzalnej atmosfery. Zapraszamy serdecznie wszystkich naszych bliskich do wspólnego świętowania.
+            </p>
           </div>
-          <div className={`column ${style.details}`}>
-            <div className={style.detailsText}>
-              <p className={style.dateTime}>
-                <strong>Data:</strong> 20 sierpnia 2024<br />
-                <strong>Godzina:</strong> 15:00
-              </p>
-              <p className={style.location}>
-                <strong>Miejsce:</strong><br />
-                Katedra w Łowiczu<br />
-                ul. Katedralna 1, 99-400 Łowicz
-              </p>
-              <p className={style.description}>
-                Ceremonia odbędzie się w pięknej Katedrze w Łowiczu, znanej ze swojej wyjątkowej architektury i niepowtarzalnej atmosfery. Zapraszamy serdecznie wszystkich naszych bliskich do wspólnego świętowania.
-              </p>
-            </div>
+          <div className={`column`}>
             <div className={style.map}>
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2450.4485010468597!2d19.941139177222222!3d52.10796787195607!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471be612c38cb329%3A0x8354b20408d76f2a!2sBazylika%20katedralna%20Wniebowzi%C4%99cia%20NMP%20i%20%C5%9Bw.%20Miko%C5%82aja%20w%20%C5%81owiczu!5e0!3m2!1spl!2spl!4v1722021914520!5m2!1spl!2spl" className="map" style={{border:0}}allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
