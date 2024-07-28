ab1b4cfec0524084b9db871c261c0e270cc01e9d
pkasprzak9 <pawkasprzak9@gmail.com>
2024-07-28 12:43:16 +0200

    ### Improve handleClickOutside performance in NavBar
    Enhanced `handleClickOutside` function by wrapping it in `useCallback` to prevent unnecessary re-renders. Optimized event listener management in `useEffect` by including `handleClickOutside` in the dependency array. Removed unused `scroll` import to clean up code. These changes improve performance and maintain code clarity.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,30 @@
-import React, { useRef, useEffect} from "react";
+import React, { useRef, useEffect, useCallback } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { Link, Element, animateScroll as scroll } from 'react-scroll';
+import { Link } from 'react-scroll';
 import styles from "./NavBar.module.scss";
 
 export default function NavBar({ isOpen, setIsOpen }) {
   const sidebarRef = useRef(null);
 
-  const handleClickOutside = (e) => {
+  const handleClickOutside = useCallback((e) => {
     if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
       setIsOpen(false);
     }
-  }
+  }, [setIsOpen]);
+
 
   useEffect(() => {
     if (isOpen) {
       document.addEventListener("mousedown", handleClickOutside);
     } else {
       document.removeEventListener("mousedown", handleClickOutside);
-    };
+    }
 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     }
-  }, [isOpen]);
+  }, [isOpen, handleClickOutside]);
 
   return (
     <nav className={styles.navbar} ref={sidebarRef}>
