e8fc78afa74d01180f8c1248d039ed1691783b28
pkasprzak9 <pawkasprzak9@gmail.com>
2024-07-28 13:31:36 +0200

    ``` Add SectionPlan component to Main.jsx
    Integrated new SectionPlan component within Main component to display the event schedule. Created associated SectionPlan styles to ensure a consistent and responsive design.
    
    This enhancement provides users with detailed planning information directly on the main page, improving the overall user experience.
    ```

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SectionCeremony from "./SectionCeremony/SectionCeremony";
 import SectionReception from "./SectionReception/SectionReception";
+import SectionPlan from "./SectionPlan/SectionPlan";
 
 export default function Main(){
     return (
         <main>
             <SectionCeremony />
             <SectionReception />
+            <SectionPlan />
         </main>
     );
 }
---
+++ src/components/Main/SectionPlan/SectionPlan.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./SectionPlan.module.scss";
+
+export default function SectionPlan() {
+    return (
+        <section className={styles.plan}>
+          <div className={styles.container}>
+            <div className={styles.card}>
+              <h2 className={styles.title}>Plan Uroczystości</h2>
+                <div className={styles.daySection}>
+                  <h3 className={styles.dayTitle}>Dzień 1</h3>
+                  <div className={styles.row}>
+                    <p className={styles.time}>15:00</p>
+                    <p className={styles.description}>Ślub</p>
+                  </div>
+                  <div className={styles.row}>
+                    <p className={styles.time}>16:00</p>
+                    <p className={styles.description}>Wesele</p>
+                  </div>
+                </div>
+                <div className={styles.daySection}>
+                  <h3 className={styles.dayTitle}>Dzień 2</h3>
+                  <div className={styles.row}>
+                    <p className={styles.time}>12:00</p>
+                    <p className={styles.description}>Poprawiny</p>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </section>
+    );
+}
