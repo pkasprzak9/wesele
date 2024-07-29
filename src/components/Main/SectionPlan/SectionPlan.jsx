20dd69a9725f5273f7c5af3b98f93806da48710f
pkasprzak9 <pawkasprzak9@gmail.com>
2024-07-29 14:47:12 +0200

    ``` Improve SectionPlan layout and style consistency
    - Added a textWrapper div for better layout separation on larger screens.
    - Adjusted the schedule time for Day 2 from 12:00 to 14:00.
    - Updated styles to use a new font for the plan section, increased font weight in the card component, and added p tag styling.
    - Applied media queries to the textWrapper for responsive design, ensuring better visual organization.
    
    Enhances readability and layout consistency across different devices.
    ```

--- src/components/Main/SectionPlan/SectionPlan.jsx
+++ src/components/Main/SectionPlan/SectionPlan.jsx
@@ -7,6 +7,7 @@ export default function SectionPlan() {
           <div className={styles.container}>
             <div className={styles.card}>
               <h2 className={styles.title}>Plan Uroczystości</h2>
+              <div className={styles.textWrapper}>
                 <div className={styles.daySection}>
                   <h3 className={styles.dayTitle}>Dzień 1</h3>
                   <div className={styles.row}>
@@ -21,12 +22,14 @@ export default function SectionPlan() {
                 <div className={styles.daySection}>
                   <h3 className={styles.dayTitle}>Dzień 2</h3>
                   <div className={styles.row}>
-                    <p className={styles.time}>12:00</p>
+                    <p className={styles.time}>14:00</p>
                     <p className={styles.description}>Poprawiny</p>
                   </div>
                 </div>
+              </div>
             </div>
           </div>
         </section>
     );
 }
+
