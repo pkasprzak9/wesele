e709ce5fe38c8a71d392a07f57f0dafd03a78b8d
pkasprzak9 <pawkasprzak9@gmail.com>
2024-07-30 17:08:36 +0200

    ``` Enhance hotel section with check-in/out details
    Added placeholders for check-in, breakfast, and check-out times to the Hotel section for better user information. Updated styles to improve layout and readability across various screen sizes with new .row class and refined card styling.
    ```

--- src/components/Main/SectionHotel/SectionHotel.jsx
+++ src/components/Main/SectionHotel/SectionHotel.jsx
@@ -8,6 +8,18 @@ export default function SectionHotel() {
         <div className={styles.card}>
           <h2 className={styles.title}>Hotel</h2>
           <p className={styles.info}>Tutaj pojawią się informacje dotyczące zakwaterowania.</p>
+          <div className={styles.row}>
+            <p className={styles.description}>Zameldowanie:</p>
+            <p className={styles.time}>-</p>
+          </div>
+          <div className={styles.row}>
+            <p className={styles.description}>Śniadanie:</p>
+            <p className={styles.time}>-</p>
+          </div>
+          <div className={styles.row}>
+            <p className={styles.description}>Wymeldowanie:</p>
+            <p className={styles.time}>-</p>
+          </div>
         </div>
       </div>
     </section>
