20204bfbaba21e8e6f9a61d75eb2462cc9089e85
pkasprzak9 <pawkasprzak9@gmail.com>
2024-08-01 17:01:01 +0200

    ``` Enhance SectionHotel layout and styling
    Refactored SectionHotel component for improved readability by grouping text elements together. Added background image to the section and adjusted text alignment based on screen size to enhance visual appeal. Center-justified row elements for smaller screens to improve layout consistency and responsiveness.
    ```

--- src/components/Main/SectionHotel/SectionHotel.jsx
+++ src/components/Main/SectionHotel/SectionHotel.jsx
@@ -7,18 +7,20 @@ export default function SectionHotel() {
       <div className={styles.container}>
         <div className={styles.card}>
           <h2 className={styles.title}>Hotel</h2>
-          <p className={styles.info}>Tutaj pojawią się informacje dotyczące zakwaterowania.</p>
-          <div className={styles.row}>
-            <p className={styles.description}>Zameldowanie:</p>
-            <p className={styles.time}>-</p>
+          <div className={styles.text}>
+            <p className={styles.info}>Tutaj pojawią się informacje dotyczące zakwaterowania.</p>
+            <div className={styles.row}>
+              <p className={styles.description}>Zameldowanie:</p>
+              <p className={styles.time}>-</p>
+            </div>
+            <div className={styles.row}>
+              <p className={styles.description}>Śniadanie:</p>
+              <p className={styles.time}>-</p>
+            </div>
+            <div className={styles.row}>
+              <p className={styles.description}>Wymeldowanie:</p>
+              <p className={styles.time}>-</p>
           </div>
-          <div className={styles.row}>
-            <p className={styles.description}>Śniadanie:</p>
-            <p className={styles.time}>-</p>
-          </div>
-          <div className={styles.row}>
-            <p className={styles.description}>Wymeldowanie:</p>
-            <p className={styles.time}>-</p>
           </div>
         </div>
       </div>
