e6f4c651a47e557a8234206508526196924a456e
pkasprzak9 <pawkasprzak9@gmail.com>
2024-10-16 12:51:18 +0200

    Update contact information

--- src/components/Main/SectionContact/SectionContact.jsx
+++ src/components/Main/SectionContact/SectionContact.jsx
@@ -9,12 +9,12 @@ export default function SectionContact() {
         <div className={styles.row}>
           <div className={`${styles.col} ${styles.text}`}>
             <span className={styles.highlight}>Telefon</span><br />
-            Joanna: <a href="tel:+48 721 108 947">+48 721 108 947</a><br />
-            Piotr: <a href="tel:+48 509 109 440">+48 509 109 440</a>
+            Joanna: <a href="tel:+123 123 123">123 123 123</a><br />
+            Piotr: <a href="tel:+321 321 321">321 321 321</a>
           </div>
           <div className={`${styles.col} ${styles.text}`}>
             <span className={styles.highlight}>E-mail</span><br />
-            <a href="mailto: wesele2024.kontakt@gmail.com" >wesele2024.kontakt@gmail.com</a>
+            <a href="mailto: supermail@mail.com" >supermail@mail.com</a>
           </div>
         </div>
       </div>
